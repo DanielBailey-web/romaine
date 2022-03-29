944231d99a448575a856c0fdbe5a9359b33576ea
castlefurnitureworks@gmail.com <castlefurnitureworks@gmail.com>
2022-03-29 16:34:09 -0700

    updated var to let

--- packages/romaine/src/util/image/multiplyMatrices.ts
+++ packages/romaine/src/util/image/multiplyMatrices.ts
@@ -1,10 +1,10 @@
 export function multiplyMatrices(m1: number[][], m2: number[][]) {
-  var result: number[][] = [];
-  for (var i = 0; i < m1.length; i++) {
+  let result: number[][] = [];
+  for (let i = 0; i < m1.length; i++) {
     result[i] = [];
-    for (var j = 0; j < m2[0].length; j++) {
-      var sum = 0;
-      for (var k = 0; k < m1[0].length; k++) {
+    for (let j = 0; j < m2[0].length; j++) {
+      let sum = 0;
+      for (let k = 0; k < m1[0].length; k++) {
         sum += m1[i][k] * m2[k][j];
       }
       result[i][j] = sum;
