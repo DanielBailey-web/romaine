3ee6a188283e56deb1f1d753682c0b6e8efd6164
castlefurnitureworks@gmail.com <castlefurnitureworks@gmail.com>
2022-04-04 16:12:49 -0700

    if image changes externall useEffect

--- packages/romaine-components/src/components/FolderSelection.tsx
+++ packages/romaine-components/src/components/FolderSelection.tsx
@@ -155,7 +155,7 @@ export const FolderSelection = ({
       window.removeEventListener("dragexit", dragExit);
     };
     // @eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [image]);
   //https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
   function dropHandler(ev: React.DragEvent<HTMLDivElement>) {
     ev.preventDefault();
