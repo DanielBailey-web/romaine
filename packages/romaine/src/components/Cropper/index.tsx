5273c97fd90ab607efa2fc422136a35511d882b0
castlefurnitureworks@gmail.com <castlefurnitureworks@gmail.com>
2022-09-05 18:40:39 -0700

    added a todo item

--- packages/romaine/src/components/Cropper/index.tsx
+++ packages/romaine/src/components/Cropper/index.tsx
@@ -2,3 +2,5 @@ export * from "./CroppingCanvas";
 export * from "./CropPoint";
 export * from "./CropPoints";
 export * from "./CropPointsDelimiters";
+
+// @todo https://jsfiddle.net/m1erickson/ytUhL/
